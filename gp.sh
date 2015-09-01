a1e4d8b428a9ea43bb8d23049e0eb7e650f7a478
Kevin Schaaf <kschaaf@google.com>
2015-08-31 23:19:13 -0700

    first commit

---
+++ gp.sh
@@ -0,0 +1,23 @@
+org=${1:-"kevinpschaaf"}
+repo=${2:-"chat-app-paper"}
+branch=${3:-"master"}
+
+rm -rf deploy
+git clone -b $branch --single-branch git@github.com:$org/$repo.git deploy
+
+pushd deploy >/dev/null
+
+git checkout -b deploy
+
+bower install
+
+polybuild index.html
+mv index.build.html index.html
+rm -rf components
+cp ../elements/threads.json .
+
+git add -A .
+git commit -am 'update gh-pages'
+git push -u origin deploy:gh-pages --force
+
+popd >/dev/null
